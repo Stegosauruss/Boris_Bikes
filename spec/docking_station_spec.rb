0d503bf4aaab57fab039d0e1ba482dd878554aed
Student <student@Makerss-MacBook-Pro.local>
2019-07-23 16:51:13 +0100

    DockingStation now releases working bikes

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,4 +1,9 @@
 require 'docking_station.rb'
 describe DockingStation do
   it { is_expected.to respond_to :release_bike }
+
+  it 'releases working bikes' do
+    bike = subject.release_bike
+    expect(bike).to respond_to :working?
+  end
 end
