25878d3b0f728a99e3dd67ca7f30eb45e98bd134
Student <student@Makerss-MacBook-Pro-2.local>
2019-07-24 16:55:47 +0100

    Added guard conditions for docked bikes

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,6 +1,31 @@
-
 class DockingStation
+    
+    attr_writer :docked_bikes
+
+    def initialize
+        @docked_bikes = []
+    end
+
+    def docked_bikes
+        @docked_bikes
+    end
+
     def release_bike
-        bike = Bike.new
+        if docked_bikes.empty? == true
+            begin
+                raise 'There is no bike'
+            end
+        else
+            bike = Bike.new
+            return bike
+        end
+    end
+
+    def dock_bike(bike)
+        if docked_bikes.size > 19
+            raise 'The docking station is full'
+        else
+            docked_bikes << bike
+        end
     end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,9 +1,34 @@
 require 'docking_station.rb'
+require 'bike'
 describe DockingStation do
+  
   it { is_expected.to respond_to :release_bike }
 
-  it 'releases working bikes' do
-    bike = subject.release_bike
+  it 'bikes have a working? method' do
+    bike = Bike.new
     expect(bike).to respond_to :working?
+  end
+
+  it 'docking station has a dock_bike(bike) method' do
+    expect(subject).to respond_to :dock_bike
+  end
+
+  it 'docked_bikes exists' do
+    expect(subject.docked_bikes.is_a?(Array)).to be true
+  end
+
+  it 'dock_bike(bike) adds an instance of bike to the instance variable bikes' do
+    subject.dock_bike(Bike.new)
+    expect(subject.docked_bikes.size).to be > 0
+  end
+
+  it 'Raise error when asking empty dockingstation to release' do
+    expect{subject.release_bike}.to raise_error('There is no bike')
+  end
+
+  it 'Raise error when asking a full dockingstation to dock' do
+    20.times {subject.dock_bike(Bike.new)}
+    bike = Bike.new
+    expect{subject.dock_bike(bike)}.to raise_error('The docking station is full')
   end
 end
